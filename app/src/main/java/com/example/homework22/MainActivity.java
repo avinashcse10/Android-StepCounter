14a8a7cb0968f829c4ac94c0d7f9329652089443
avinashcse10 <avinash.cse10@gmail.com>
2020-04-23 17:19:31 -0400

    Initial commit

---
+++ app/src/main/java/com/example/homework22/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.homework22;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Content displayed as Fragment from MainActivityFragment class.
+    }
+}
